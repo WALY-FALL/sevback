9c2471f4d19bb113fdba4feddc7f727f41b3ae2f
Waly FALL <2atelecompwf@gmail.com>
2025-12-07 17:53:43 +0000

    deployed

--- server.js
+++ server.js
@@ -1,4 +1,52 @@
-import "dotenv/config";   // Bibliotéque de node.js. Permet de charger les variables d'environnement
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import profRoutes from "./routes/profRoutes.js";
+import eleveRoutes from "./routes/eleveRoutes.js";
+import classRoutes from "./routes/classroutes.js";
+import coursRoutes from "./routes/coursRoutes.js";
+import demandeRoutes from "./routes/demandeRoutes.js";
+import connectDB from "./config/db.js";
+import path from "path";
+
+const app = express();
+
+// ✅ CORS UNIFIÉ
+app.use(cors({
+  origin: "https://senecolevirtuelle.vercel.app",
+  credentials: true,
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+}));
+
+// 🔥 Répondre aux requêtes OPTIONS (preflight)
+app.options("*", cors());
+
+const __dirname = path.resolve();
+
+// DB
+connectDB();
+
+// Middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+
+// Routes
+app.use("/api/eleves", eleveRoutes);
+app.use("/api/profs", profRoutes);
+app.use("/api/classes", classRoutes);
+app.use("/api/cours", coursRoutes);
+app.use("/api/demandes", demandeRoutes);
+
+// Uploads
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Serveur
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
+
+/*import "dotenv/config";   // Bibliotéque de node.js. Permet de charger les variables d'environnement
 import express from "express"; // framework de node.js. Sert uniquement á gérer les routes http comm app.get, app.post ...
 import cors from "cors";  // Middleware permet d'activer le CROSS-ORIGIN RESOURCE SHARING
 import morgan from "morgan";//Middlewae du protocole http. permet de loguer dans le cosole toutes les requètes http
@@ -85,5 +133,5 @@ app.use("/api/demandes", demandeRoutes);
 
 
 // Démarrage du serveur
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+//const PORT = process.env.PORT || 5000;
+//app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
