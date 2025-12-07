b636463c8cd78f48cddf197814bdb2cf011158f6
Waly FALL <2atelecompwf@gmail.com>
2025-12-07 18:11:47 +0000

    deployed

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors({
 app.use(express.json());
 
 // Répondre proprement aux preflight OPTIONS
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   if (req.method === "OPTIONS") {
     res.setHeader("Access-Control-Allow-Origin", "https://senecolevirtuelle.vercel.app");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
     return res.status(200).end();
   }
   next();
-});
+});*/
 
 /*app.use(cors({
   origin: "https://senecolevirtuelle.vercel.app",
